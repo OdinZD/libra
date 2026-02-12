089b97321a035ea3d05f91639e3dea7de81758e3
OdinZD <odin.perica@gmail.com>
2026-02-12 10:02:08 +0100

    First commit for Libra website

---
+++ resources/views/components/app-logo-icon.blade.php
@@ -0,0 +1,13 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" {{ $attributes }}>
+    <!-- Libra scales icon -->
+    <line x1="12" y1="2" x2="12" y2="22" />
+    <path d="M4 10l4-6" />
+    <path d="M20 10l-4-6" />
+    <line x1="4" y1="4" x2="20" y2="4" />
+    <path d="M1 14h6" />
+    <path d="M17 14h6" />
+    <path d="M1 14l3-4 3 4" />
+    <path d="M17 14l3-4 3 4" />
+    <path d="M1 14a3 3 0 0 0 6 0" />
+    <path d="M17 14a3 3 0 0 0 6 0" />
+</svg>
